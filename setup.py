0af9c593aacdff5ad9c6dcc512c1aa5d2c5a1adc
alexyskoutnev <alexyskoutnev@gmail.com>
2024-01-13 15:30:11 -0600

    setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Humanoid_MARL',  # Replace with your package name
+    version='0.0.1',  # Replace with your package version
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
